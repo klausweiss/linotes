e2bfdaf0963310608e5416a05b0755c8bdb13da0
Mikołaj Biel <biel.mikolaj@openmailbox.org>
2016-12-31 11:57:57 +0100

    Display box with placeholder note content

--- app/Notes.hs
+++ app/Notes.hs
@@ -3,63 +3,80 @@ module Main where
 
 import Control.Monad (void)
 import Control.Monad.IO.Class (liftIO)
+import Data.Monoid ((<>))
 
-import qualified Brick.AttrMap as AttrMap
-import qualified Brick.Main as Main
+import qualified Brick.AttrMap as AM
+import qualified Brick.Main as M
 import Brick.Types
     ( Widget
+    , Size(..)
+    , getContext
+    , availWidthL
+    , render
     )
 import Brick.Util (on)
-import qualified Brick.Widgets.Border as Border
+import qualified Brick.Widgets.Border as B
 import Brick.Widgets.Core
     ( (<+>)
     , vBox
     , hLimit
     , vLimit
     , str
+    , padAll
     )
-import qualified Brick.Types as Types
-import qualified Brick.Widgets.List as List
+import qualified Brick.Types as T
+import qualified Brick.Widgets.List as L
 import qualified Data.Vector as Vec
 import qualified Graphics.Vty as Vty
+import Lens.Micro ((^.))
 
 import DatabaseController (readNotes)
 
 main :: IO ()
 main = do
     notes <- readNotes
-    void $ Main.defaultMain app (initialState notes)
+    void $ M.defaultMain app (initialState notes)
 
-app :: Main.App (List.List () String) e ()
-app = Main.App { Main.appDraw = drawUI
-               , Main.appStartEvent = return
-               , Main.appChooseCursor = Main.showFirstCursor
-               , Main.appHandleEvent = appEvent
-               , Main.appAttrMap = const attrMap
-               }
+app :: M.App (L.List () String) e ()
+app = M.App { M.appDraw = drawUI
+            , M.appStartEvent = return
+            , M.appChooseCursor = M.showFirstCursor
+            , M.appHandleEvent = appEvent
+            , M.appAttrMap = const attrMap
+            }
 
-drawUI :: (Show a) => List.List () a -> [Widget ()]
+drawUI :: (Show a) => L.List () a -> [Widget ()]
 drawUI _list = [ui] where
-    ui = vBox [box]
-    box = Border.borderWithLabel (str "Notes") $
-        hLimit 25 $
-        vLimit 5 $
-        List.renderList listElemRenderer True _list
+    ui = vBox [ notes
+              , noteContent]
+    notes = B.borderWithLabel (str "Notes") $
+        L.renderList listElemRenderer True _list
+    noteContent = B.borderWithLabel (str "Content") $
+            noteContainer "Tresc notatki"
+
+noteContainer :: String -> Widget n
+noteContainer content = 
+    T.Widget Greedy Greedy $ do
+        ctx <- getContext
+        maxW <- return $ ctx ^. availWidthL
+        render $ 
+            hLimit maxW $
+            str $ content ++ (take maxW $ repeat ' ')
 
 listElemRenderer :: (Show a) => Bool -> a -> Widget ()
 listElemRenderer selected elem = str $ show elem
 
-appEvent :: List.List () String -> Types.BrickEvent () e -> Types.EventM () (Types.Next (List.List () String))
-appEvent _list (Types.VtyEvent e) =
+appEvent :: L.List () String -> T.BrickEvent () e -> T.EventM () (T.Next (L.List () String))
+appEvent _list (T.VtyEvent e) =
     case e of
-        Vty.EvKey Vty.KEsc [] -> Main.halt _list
-        ev -> Main.continue =<< List.handleListEvent ev _list
+        Vty.EvKey Vty.KEsc [] -> M.halt _list
+        ev -> M.continue =<< L.handleListEvent ev _list
 
-attrMap :: AttrMap.AttrMap
-attrMap = AttrMap.attrMap Vty.defAttr
-    [ (List.listAttr,         Vty.white `on` Vty.blue)
-    , (List.listSelectedAttr, Vty.red `on` Vty.white)
+attrMap :: AM.AttrMap
+attrMap = AM.attrMap Vty.defAttr
+    [ (L.listAttr,         Vty.white `on` Vty.blue)
+    , (L.listSelectedAttr, Vty.red `on` Vty.white)
     ]
 
-initialState :: [String] -> List.List () String
-initialState _list = List.list () (Vec.fromList _list) 1
+initialState :: [String] -> L.List () String
+initialState _list = L.list () (Vec.fromList _list) 1
