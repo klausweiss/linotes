2eee031c1f71dbf1561e6b5684f02d1845bc03ec
Mikołaj Biel <biel.mikolaj@openmailbox.org>
2016-12-31 02:16:14 +0100

    Add adding notes to database

---
+++ src/DatabaseController.hs
@@ -0,0 +1,38 @@
+{-# LANGUAGE EmptyDataDecls             #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE GADTs                      #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE QuasiQuotes                #-}
+{-# LANGUAGE TemplateHaskell            #-}
+{-# LANGUAGE TypeFamilies               #-}
+
+module DatabaseController
+    ( saveNote
+    , readNotes
+    ) where
+
+import Database.Persist
+import Database.Persist.Sqlite hiding (migrate)
+import Database.Persist.TH
+import Data.Time
+import Control.Monad.IO.Class
+
+import Models.Note
+
+db_file = "notes.sqlite3"
+
+
+saveNote :: String -> IO String
+saveNote note = runSqlite db_file $ do
+    runMigration migrateAll
+    time <- liftIO getCurrentTime
+    noteId <- insert $ Note note time
+    return note
+
+readNotes :: IO [String]
+readNotes = runSqlite db_file $ do
+    runMigration migrateAll
+    notes <- selectList [] []
+    return $ map (noteContent . entityVal) notes
---
+++ src/Models/Note.hs
@@ -0,0 +1,26 @@
+-- tutorial:
+-- http://stackoverflow.com/a/27220836/3103257
+
+{-# LANGUAGE EmptyDataDecls             #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE GADTs                      #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE QuasiQuotes                #-}
+{-# LANGUAGE TemplateHaskell            #-}
+{-# LANGUAGE TypeFamilies               #-}
+
+module Models.Note where
+
+import Data.Time 
+import Database.Persist
+import Database.Persist.Sqlite
+import Database.Persist.TH
+
+share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
+Note
+    content String
+    lastModified UTCTime default=CURRENT_TIME
+    deriving Show
+|]
